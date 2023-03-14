e1d1efc36ec0eb440d54df0e1cb72b39eee18cf1
Mohammed Arshad <marsha6@uic.edu>
2023-03-14 16:04:56 -0500

    Cursor Selection Updated

--- components/navBtns.js
+++ components/navBtns.js
@@ -1,5 +1,7 @@
 export default function NavBtns({children}){
     return (
-        <p className="text-white p-3 text-2xl text-right justify-items-end test">{children}</p>
+        <p className="text-white px-5 py-2 text-2xl text-left justify-items-end test
+       "><span className=" hover:underline transition-all
+       hover:cursor-pointer select-none">{children}</span></p>
     )
 }
--- components/rounded-button.js
+++ components/rounded-button.js
@@ -5,6 +5,7 @@ export default function RoundedButton({children}){
          px-5 py-2 pb-3 m-5
          border-white border-solid border-2
           hover:bg-slate-900 transition-colors
+          hover:cursor-pointer select-none
           text-2xl
           ">{children}</div>
     )
