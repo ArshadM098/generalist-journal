0caf4d2453a6c11050c8fb8057abf3732e7258f9
Mohammed Arshad <marsha6@uic.edu>
2023-03-17 17:54:02 -0500

    Added Gallery Component

---
+++ app/subfolders/gallery.js
@@ -0,0 +1,8 @@
+
+export default function Gallery(){
+    return(
+        <div className="rounded-md border-1 border-solid border-white
+                        w\        
+        "></div>
+    )
+}
