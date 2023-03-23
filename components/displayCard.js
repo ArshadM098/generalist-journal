deb8a00e2fa6c521a20b39492fbcd2c44c987589
ArshadM098 <marsha6@uic.edu>
2023-03-23 15:29:46 -0500

    Updated Display Card

--- components/displayCard.js
+++ components/displayCard.js
@@ -1,15 +1,25 @@
+"use client";
 import RoundedButton from "./rounded-button";
+import { useState } from "react"; // Import the CSS file for DisplayCard
+
 export default function DisplayCard({ children, imageUrl }) {
-    return (
-      <div
-          className={` border-slate-700 border-[1px] rounded-lg border-solid overflow-hidden block w-[400px] h-[250px] bg-cover bg-center bg-no-repeat`}
-          style={{ backgroundImage: `url(${imageUrl})` }}>
-          <div className=" relative h-full w-full backdrop-blur-[2px] transition-all">
-            <div className="absolute text-white text-2xl font-bold m-5">{children}</div>
-            <div className="absolute bottom-0 right-0">
-            <RoundedButton>Check it out!</RoundedButton></div>
-          </div>
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className={`border-slate-700 border-[1px] rounded-lg border-solid overflow-hidden block w-[400px] h-[250px] bg-cover bg-center bg-no-repeat display-card`}
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      onMouseEnter={() => {setIsHovered(true);}}
+      onMouseLeave={() => {setIsHovered(false);}}
+    >
+      <div className="relative h-full w-full backdrop-blur-[2px] transition-all">
+        <div className="absolute text-white text-2xl font-bold m-5">{children}</div>
+        <div className={`absolute bottom-0 right-0 transition-opacity ${isHovered ? "opacity-100" : "opacity-0"}`}>
+          <RoundedButton c>
+            Proceed
+          </RoundedButton>
         </div>
-    );
-  }
-  
+      </div>
+    </div>
+  );
+}
