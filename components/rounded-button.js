0aacb19c9ed63bfd3d7df377cf08cbaa08131c79
Mohammed Arshad <marsha6@uic.edu>
2023-03-28 17:16:20 -0500

    Fixed Button Link

--- components/rounded-button.js
+++ components/rounded-button.js
@@ -1,15 +1,20 @@
 import Link from "next/link"
 export default function RoundedButton({children, link="/"}){
     return(<>
+        <Link href={link}>
         <div className=" block relative rounded-full h-fit w-fit z-auto
          px-5 py-2 pb-3 m-2
          border-white border-solid border-2
          bg-black
-          hover:bg-red-900 transition-colors
-          hover:cursor-pointer select-none
-          text-1xl
-          backdrop-brightness-50
-          "><Link href={link}>{children}</Link></div>
+         hover:bg-gradient-to-br    hover:from-orange-500 hover:to-red-700 shadow-2xl transition-colors
+        active:bg-orange-900
+        hover:cursor-pointer select-none
+        text-1xl
+        backdrop-brightness-50
+          "
+          style={{boxshadow: `28px 28px 56px #e6733a, 
+          -28px -28px 56px #ff8d46`}}
+          >{children}</div></Link>
           </>
     )
 }
