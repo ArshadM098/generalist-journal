78394d25a00097d1efd8fa7b3ab4618b9b3392cf
Mohammed Arshad <marsha6@uic.edu>
2023-03-31 15:18:01 -0500

    TMS Setup

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,23 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+      {/* <!-- Google Tag Manager --> */}
+      <script>{function(w,d,s,l,i){
+                    w[l]=w[l]||[];
+                    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                    var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                    j.async=true;
+                    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                    f.parentNode.insertBefore(j,f);
+                    }(window,document,'script','dataLayer','GTM-TBTW8D4')}</script>
+              {/* <!-- End Google Tag Manager --> */}
+      </head>
       <body className='bg-black text-white overflow-scroll-y overflow-x-hidden'>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTW8D4"
+         height="0" width="0" style="display:none;visibility:hidden">
+        </iframe></noscript>
         
         <div  className='z-10 py-24'>{children}</div>
         <div className={`fixed top-0 left-0 bg-black -z-10 transition-all h-full w-full ${showDropdown ? 'z-30 opacity-70 ':'-z-10 opacity-0'}`} onClick={()=> setShowDropdown(false)}></div>
