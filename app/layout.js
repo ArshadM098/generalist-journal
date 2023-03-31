d4fb5d969830ce9d99b1319addd0a2b51260bd40
Mohammed Arshad <marsha6@uic.edu>
2023-03-31 15:21:29 -0500

    Changes to fix TMS

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import { useState, useRef, useEffect } from 'react';
 import './globals.css';
 import { NavBar, HamBtn } from "@/components/navBar";
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   
@@ -12,16 +13,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
       {/* <!-- Google Tag Manager --> */}
-      <script>{function(w,d,s,l,i){
+      
+              {/* <!-- End Google Tag Manager --> */}
+      </head>
+      <Script>{`function(w,d,s,l,i){
                     w[l]=w[l]||[];
                     w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
                     var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
                     j.async=true;
                     j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
                     f.parentNode.insertBefore(j,f);
-                    }(window,document,'script','dataLayer','GTM-TBTW8D4')}</script>
-              {/* <!-- End Google Tag Manager --> */}
-      </head>
+                    }(window,document,'script','dataLayer','GTM-TBTW8D4')`}
+      </Script>
       <body className='bg-black text-white overflow-scroll-y overflow-x-hidden'>
       <noscript>
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTW8D4"
