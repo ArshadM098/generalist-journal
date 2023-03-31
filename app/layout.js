ae975eda61fde092ba7f5047c25a29d51dbe3c47
Mohammed Arshad <marsha6@uic.edu>
2023-03-31 15:24:38 -0500

    Changed Strategy

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       
               {/* <!-- End Google Tag Manager --> */}
       </head>
-      <Script>{`function(w,d,s,l,i){
+      <Script strategy='afterInteractive'>{`function(w,d,s,l,i){
                     w[l]=w[l]||[];
                     w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
                     var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
