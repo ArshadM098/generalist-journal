a55abb282b1bd14736792df8d742a60ee5fe32de
Mohammed Arshad <marsha6@uic.edu>
2023-03-31 15:34:45 -0500

    Probably the last fix for TMS

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from 'react';
 import './globals.css';
 import { NavBar, HamBtn } from "@/components/navBar";
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   
@@ -11,22 +12,18 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
-      {/* <!-- Google Tag Manager --> */}
-      
-              {/* <!-- End Google Tag Manager --> */}
-      </head>
-      <Script strategy='afterInteractive'>
-        {function(w,d,s,l,i){
-                    w[l]=w[l]||[];
-                    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                    var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                    j.async=true;
-                    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                    f.parentNode.insertBefore(j,f);
-                    }(window,document,'script','dataLayer','GTM-TBTW8D4')}
-                    
-      </Script>
+      <Head>
+          <S  cript dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TBTW8D4');`,
+            }}></S>
+      </Head>
       <body className='bg-black text-white overflow-scroll-y overflow-x-hidden'>
       <noscript>
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTW8D4"
