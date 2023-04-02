c25249eefd54d883a13712999994800ba703c61b
Mohammed Arshad <marsha6@uic.edu>
2023-04-01 22:31:40 -0500

    Changes to resumeComponents

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,22 +1,61 @@
-import { Work } from "@/components/resumeComponents"
+import { Experience } from "@/components/resumeComponents"
 
 export default function Home(){
     return (
         <>
-            <Work company="UIC Department of ECE" start="Jan 2023" end="March 2023" link="/image-denoising">
+            <Experience start="Sep 2022" end="Dec 2022" link="/image-denoising">
+                <>Adaptive Filter Design for Image Denoising Using Reinforcement Learning</>
+                <>
+                    <li>Proposed an approach for designing an Adaptive Filter using Reinforcement Learning
+                    Mechanisms to perform image denoising utilizing the principles of digital signal processing.</li>
+                    <li>Developed a reinforcement learning model that can learn to build an adaptive 
+                    filter for image denoising by modeling the problem to fit an agent-environment scenario.</li>
+                    <li>Utilized DSP operations such as Fast-Fourier Transform and Frequency Peak Detection to
+                         achieve the expected results of Image denoising.</li>
+                </>
+                <>Python,C++,Arduino,Linux,JavaScript</>
+            </Experience>
+
+            <Experience start="Mar 2022" end="May 2022">
+                <>Multiplication and Accumulation Datapath for Neural Networks</>
+                <>
+                    <li>Designed MAC datapath in cadence utilizing CMOS logic gates to develop 8-bit Adder, Multiplier, Flip-Flops, Register, SRAM cell and Decoder.</li>
+                    <li>Performed extensive simulation and testing to determine the effects of change in environment on logic gates.</li>
+                </>
+            </Experience>
+            
+            <Experience start="Nov 2021" end="Dec 2021">
+                <>Wrist-Aid (Medical Aid Device)</>
+                <>
+                    <li>Developed a novel medical aid device for the prevention of repetitive strain injuries using flex resistors.</li>
+                    <li>Implemented wireless data transmission between wearable and main system to enable real-time UI.</li>
+                    <li>Created an application using Processing 3 (Java) to extrapolate sensor data and provide a visual guide for the wearable and its health statistics.</li>
+                </>
+            </Experience>
+
+            <Experience start="Aug 2021" end="Dec 2021" >
+                <>AnSRR (Autonomous Snow Removal Robot)</>
+                <>
+                    <li>Led AGILE development of autonomous snow removal robots with a senior design engineering team.</li>
+                    <li>Successfully demonstrated path finding algorithm in Gazebo simulation using ROS.</li>
+                </>
+            </Experience>
+
+            <Experience company="UIC Department of ECE"  start="May 2019" end="Aug 2019">
+                <>NSF Research Experience for Undergraduates</>
+                <>
+                    <li>Developed and implemented a quadcopter flight stabilization algorithm on the FRDM-KL25Z board.</li>
+                    <li>Utilizing scheduled callbacks and software interrupts in C++ for improved performance.</li>
+                </>
+            </Experience>
+            {/* 
+            <Experience company="UIC Department of ECE" start="Jan 2023" end="March 2023" link="/image-denoising">
                 <>NSF Research Experience for Undergraduates</>
                 <>
                 <li>Developed and implemented a quadcopter flight stabilization algorithm on the FRDM-KL25Z board.</li>
                 <li>Utilizing scheduled callbacks and software interrupts in C++ for improved performance.</li>
                 </>
-            </Work>
-            <Work>
-                <>NSF Research Experience for Undergraduates</>
-                <>
-                <li>Developed and implemented a quadcopter flight stabilization algorithm on the FRDM-KL25Z board.</li>
-                <li>Utilizing scheduled callbacks and software interrupts in C++ for improved performance.</li>
-                </>
-            </Work>
+            </Experience> */}
         </>
     )
 }
--- components/resumeComponents.js
+++ components/resumeComponents.js
@@ -1,24 +1,43 @@
 import Link from "next/link";
-
-export function Work({children,company=null,start=null,end=null,link=null}){
-    var hasCompany = company != null;
+export function Experience({children,company=null,start=null,end=null,link=null}){
+    var hasCompany = company != null ;
     var hasStart = start != null;
     var hasEnd = end != null;
     var hasLink = link != null;
+    var hasSkills = children[2] != undefined; 
+    var skillTags = [];
+    if (hasSkills){
+        skillTags= children[2].props.children.split(',');
+    }  
     return (
-        <div className="relative flex flex-col bg-zinc-900 p-5 rounded-md">
-            <div className=" text-2xl font-bold test mr-64 first-letter:text-3xl uppercase">{children[0]}
-            {
-                hasLink && <Link href={hasLink ? link : "/resume"} className="ml-2 text-3xl text-slate-500 hover:text-orange-500 transition-all cursor-pointer">&#8690;</Link>
-            }
-            </div>
-            {hasCompany && <div className="text-slate-500 mr-64 test uppercase">{company}</div>}
-            {hasStart && <div className="absolute right-0 top-0 text-right m-5">
-               <span>{start}</span>
-                {hasEnd && <span> - {end}</span>}
+        <div className="relative mr-5 ">
+        
+        <div className="relative flex bg-zinc-900 p-5 rounded-md flex-row flex-wrap gap-2">
+                <div className=" text-2xl font-bold test first-letter:text-3xl uppercase lg:mr-40 lg:mt-0 mt-10 w-full">{children[0]}
+                {hasLink && <Link href={hasLink ? link : "/resume"}
+                className="ml-2 text-3xl text-slate-500 hover:text-orange-500 transition-all cursor-pointer">&#8690;</Link>}
+                </div>
+                {hasCompany && <div className="text-slate-500 test uppercase">{company}</div>}
+                <ul className=" test px-5 font-light border-r-[1px] border-slate-700 text-xl list-circle space-y-4 w-full">{children[1]}</ul>
+                { hasSkills && <div id="skills" className=" w-full border-zinc-800 p-2 border-[1px] rounded-md flex flex-row flex-wrap gap-2">
+                {skillTags.map((skillTag) => (
+                        <SkillTags key={skillTag}>{skillTag}</SkillTags>
+                    ))}
+                </div>}
+
+        </div>
+        {hasStart && <div className=" absolute text-right top-2 -right-5 min-w-64 min-w-fit bg-orange-700 px-3 py-1 shadow-sm shadow-black">
+               <span>{start}{hasEnd && ` - ${end}`}</span>
             </div>}
-            
-            <ul className="pl-5 mt-5 font-light border-r-[1px] border-slate-700 text-xl mr-64 list-circle ">{children[1]}</ul>
         </div>
     )
 }
+
+
+export function SkillTags({ children }) {
+    return (
+      <div className=" inline text-sm relative w-fit border-[1px] rounded-full px-4 pt-[1px] uppercase pb-[3px]  text-orange-400  border-orange-400 border-solid">
+       {children}
+      </div>
+    );
+  }
